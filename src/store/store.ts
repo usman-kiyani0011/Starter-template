8795168eb105d371d8459c0145feac7291d68261
Usman Kiyani <usman.kiyani@ceative.co.uk>
2022-08-25 05:30:20 +0500

    #comment first commit

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+const store = configureStore({
+  reducer: {},
+});
+
+export type AppDispatch = typeof store.dispatch;
+// Export a hook that can be reused to resolve types
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
